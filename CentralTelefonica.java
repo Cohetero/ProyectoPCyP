adf213d78d24df2e992150a58ab44a59bf06635c
Komsal <62627872+Mario-Encinas@users.noreply.github.com>
2020-04-29 22:15:02 -0500

    Codigo clase main comentado.

--- CentralTelefonica.java
+++ CentralTelefonica.java
@@ -1,15 +1,26 @@
-//Main del proyecto central telefonica, la clase main unicamente instanciará a las clases Telefono y Central
+/***************************************\
+*Main del proyecto central telefonica,  *
+*la clase main unicamente instanciará a *
+*las clases necesarias e inicia la      *
+*ejecucion de los procesos.             *
+\***************************************/
+
 public class CentralTelefonica
 {
     public static void main (String args[])
     {
+        //------------------Creacion de instancias-----------------
+        //Se crea un array para diez elementos de tipo "Telefono"
         Telefono[] telefonos = new Telefono[10]; 
         
+        //Se instancia cada uno de los elementos de dicho array
         for(int i = 0; i < 10; i++) telefonos[i] = new Telefono();
         
+        //Se instancia un elemento "Central" 
         Central central = new Central(telefonos);
         
+        //-----------------Ejecucion de los hilos-----------------
         central.start();
-        for(Telefono telefono: telefonos) telefono.start();
+        for(Telefono telefono: telefonos) telefono.start();//se ejecuta un hilo por cada instancia "telefono" en "telefonos" 
     }
 }
