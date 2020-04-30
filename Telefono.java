882d450b692194fb07de81413b7d037eacb75ab8
Komsal <62627872+Mario-Encinas@users.noreply.github.com>
2020-04-29 22:27:21 -0500

    Update Telefono.java

--- Telefono.java
+++ Telefono.java
@@ -1,8 +1,17 @@
+/***************************************\
+*Clase Telefono de proyecto "Central	*
+*Telefonica"				*
+*Su funcion es emular el funncionamiento*
+*de un telefono. Podrá realizar y     	*
+* recibir llamadas			*
+\***************************************/
+
 public class Telefono extends Thread{
 	
-	public Telefono(){
-		
-	}
+	
+	
+	//Constructor de telefono, no requiere parametros al ser creado.
+	public Telefono(){}
 
 	public void run() {
 		System.out.println("soy un telefono");
