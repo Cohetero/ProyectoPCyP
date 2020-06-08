0551d0ac3ef2dc3bde38af8b334a7ac2ca3d97ab
Cohetero <48872037+Cohetero@users.noreply.github.com>
2020-06-08 18:18:27 -0500

    Update CentralTelefonica.java

--- CentralTelefonica.java
+++ CentralTelefonica.java
@@ -1,36 +1,38 @@
-/***************************************\
-*Main del proyecto central telefonica,  *
-*la clase main unicamente instanciará a *
-*las clases necesarias e inicia la      *
-*ejecucion de los procesos.             *
-\***************************************/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package centraltelefonica;
 
-public class CentralTelefonica
-{
-    public static void main (String args[])
-    {
-        //------------------Creacion de instancias-----------------
-        //Se crea un array para diez elementos de tipo "Telefono"
-        Telefono[] telefonos = new Telefono[10]; 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author M1-A1
+ */
+public class CentralTelefonica extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
-        //Se instancia un objeto "Central" 
-        Central central = new Central(telefonos);
+        Scene scene = new Scene(root);
         
-        //se instancia la interfaz de usuario
-        InterfazUsuario interfaz = new InterfazUsuario(telefonos);
-        
-        //Se instancia cada uno de los elementos de dicho array
-        for(short i = 0; i < 10; i++) telefonos[i] = new Telefono(central, i);
-        
-        //-----------------Ejecucion de los hilos-----------------
-//<<<<<<< Codigo-en-revison-2
-//=======
-        central.start();
-//>>>>>>> Codigo-en-revision
-        for(Telefono telefono: telefonos) telefono.start();//se ejecuta un hilo por cada instancia "telefono" en "telefonos"
-        
-        while(true){
-        	interfaz.interfaz();
-        }
+        stage.setTitle("Central telefonica");
+        stage.setScene(scene);
+        stage.show();
     }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
 }
