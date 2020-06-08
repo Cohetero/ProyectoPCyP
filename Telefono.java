e454a27b783ef103eff101df65ee9422607a3ede
YISUS9828 <63568650+YISUS9828@users.noreply.github.com>
2020-06-08 05:28:27 -0500

    Update Telefono.java

--- Telefono.java
+++ Telefono.java
@@ -144,29 +144,21 @@ public class Telefono extends Thread{
 		System.out.println("marcado al tel No: "+numero);
 	}
 	
-	public synchronized  void colgar() {
+	public synchronized void colgar() {
 		//Comparar si el telefono descolgado no esta realizando una llamada.
-		//Esto para poder notificar al otro telefono que la llamada ya ha finalizado.
+		//Esto para poder notificar al otro teléfono que la llamada ya ha finalizado.
 		System.out.println("Voy a colgar...");
-		//Escenarios fuera de una llamada con otro telefono (3) o bien, este permanezca colgado pero esta siendo llamado (6) 
-		if(this.getEstado() > 0 && this.getEstado() != 3) {
+		//Escenarios fuera de una llamada con otro teléfono (3) o bien, este permanezca colgado pero está siendo llamado (6) 
+		if(this.getEstado() > 1 && this.getEstado() < 5) {
 			System.out.println("Este telefono se ha colgado");
-			this.setEstado(0); // El estado de colgado es 0. 
-			//Deteniendo flujo del telefono.
-			dormido = true; //el telefono ha sido colgado.
-            		this.numero = -1; //se asigna -1 debido a que el telefono 0 existe. 
-            		this.start(); //renaudando el flujo de la informacion en el telefono despues de haber colgado.
+			this.dormir();
 		}
-		//De lo contrario, si el telefono esta en una llamada es necesario notificar a la central que el telefono esta libre.
-		else if(this.getEstado() == 3) {
+		//De lo contrario, si el teléfono está en una llamada es necesario notificar a la central que el teléfono esté libre.
+		else if(this.getEstado() == 5) {
 			//se debe notificar a la central para que el otro telefono sepa que ya se ha finalizado la llamada
 			central.finalizarLlamada(telefono, numero); //donde telefono es el remitente y numero es el telefono con el que se comunica
 			System.out.println("Este telefono con numero " + numero + " finalizo la llamada con " + telefono);
-			this.setEstado(0);
-			dormido = true;
-            		this.numero = -1; //se asigna -1 debido a que el telefono 0 existe.
-            		notifyAll(); //el telefono esta libre y puede realizarse una nueva llamada
-			this.start(); //renaudando el flujo de la informacion en el telefono despues de haber colgado.
+			this.dormir();
 		}
 	}
 	
@@ -197,6 +189,18 @@ public class Telefono extends Thread{
 		notify();//llamada a la funcion que reanuda la ejecucion del hilo.
 	}
 
+	//---------funcion que renauda el hilo-----------------------
+        //una vez que el teléfono haya colgado, es necesario renaudar la ejecución del
+        //hilo para que la información de este vuelva a fluir. Esta es llamada a través de 
+        //la funcion colgar();
+        private synchronized void dormir() {
+            this.setEstado(0);//estado cambia a 0, el cual indica que el teléfono ha colgado.
+            this.numero = -1; //no hay teléfono con el que se establezca una comunicación
+            dormido = true;//la bandera indica que el teléfono se ha colgado
+            System.out.println("Me dormi -> " + telefono);
+            notify(); //renauda la ejecución del hilo.
+        }
+	
 	public synchronized boolean getDormido(){
 		return dormido;
 	}
